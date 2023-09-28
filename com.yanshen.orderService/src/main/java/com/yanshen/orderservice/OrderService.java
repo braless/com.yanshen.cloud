c2233981ac84228fbf3dc6edd11bcbac18aa1a5d
崔彦超 <balabachao@gmail.com>
2023-09-28 14:38:57 +0800

    整合mq

--- com.yanshen.orderService/src/main/java/com/yanshen/orderservice/OrderService.java
+++ com.yanshen.orderService/src/main/java/com/yanshen/orderservice/OrderService.java
@@ -2,10 +2,11 @@ package com.yanshen.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = RedisAutoConfiguration.class)
 @EnableFeignClients
 @EnableDiscoveryClient
 public class OrderService {
