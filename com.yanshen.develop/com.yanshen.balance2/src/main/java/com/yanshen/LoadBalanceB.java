7fb2ede28d8af2f4acb64ed15c19ce3a6ec9b2d3
崔彦超 <balabachao@gmail.com>
2024-01-17 17:04:32 +0800

    调整nacos

--- com.yanshen.develop/com.yanshen.balance2/src/main/java/com/yanshen/LoadBalanceB.java
+++ com.yanshen.develop/com.yanshen.balance2/src/main/java/com/yanshen/LoadBalanceB.java
@@ -2,12 +2,13 @@ package com.yanshen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = RedisAutoConfiguration.class)
 //@EnableEurekaClient
 @EnableFeignClients
 @RefreshScope
